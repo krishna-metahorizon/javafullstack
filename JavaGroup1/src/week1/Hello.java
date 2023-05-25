aaa80f8a7f51c936e868c495ae8559be42cf934b
Krishna Aryal <krishna.metahorizon@gmail.com>
2023-05-25 17:43:32 +0545

    May 25 2023 5:43 PM

--- JavaGroup1/src/week1/Hello.java
+++ JavaGroup1/src/week1/Hello.java
@@ -1,10 +1,6 @@
-package week1;
-
-public class Hello {
-	public static void main(String []args) {
-		System.out.print("Hello\n");
-		System.out.print("Hi\n");
-		System.out.println("How are you?");
-		System.out.println("Thank you!");
+public class Hello{
+	public static void main(String []args){
+		System.out.println("Hello World of Java");
+		System.out.println("Hi");
 	}
 }
