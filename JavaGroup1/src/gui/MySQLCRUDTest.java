2180bf522872dcfa2bbe9b43335db2ceb6771116
Krishna Aryal <krishna.metahorizon@gmail.com>
2023-06-20 17:39:12 +0545

    June 20 2023 5:39 PM

---
+++ JavaGroup1/src/gui/MySQLCRUDTest.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MySQLCRUDTest extends JFrame{
+
+	public MySQLCRUDTest() {
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new MySQLCRUDTest();
+	}
+
+}
